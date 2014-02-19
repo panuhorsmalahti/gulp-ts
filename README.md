da5c22d79811d1473c81539624d7166d5368c585
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2014-02-19 19:48:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+gulp-ts
+=========
+
+TypeScript compiler plugin for Gulp.
+
+Usage:
+```javascript
+var ts = require('gulp-ts');
+
+```
+
---
+++ index.js
@@ -0,0 +1,26 @@
+/*jshint node:true */
+
+// Requires
+var map = require('map-stream');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+'use strict';
+
+
+var tsPlugin = function (options) {
+    return map(function (file, cb) {
+      // Null values and streams are skipped
+      if (file.isNull()) {
+        return cb(null, file);
+      } else if(file.isStream()) {
+        return cb(new PluginError('gulp-ts', 'Streaming not supported'));
+      }
+
+      
+
+    });
+};
+
+
+module.exports = tsPlugin;
