e89d352d4d0f5179ea7fac1daeacdbade43f093f
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2014-02-19 20:33:46 +0200

    More work on the compiler

--- index.js
+++ index.js
@@ -4,12 +4,19 @@
 var map = require('map-stream');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
+var through = require('through');
 
 'use strict';
 
 
 var tsPlugin = function (options) {
-    return map(function (file, cb) {
+    var tscPath = 'node_modules/typescript/bin/tsc',
+        bufferFiles,
+        compileFiles;
+
+    // Collect all files to an array
+    var files = [];
+    bufferFiles = function (file) {
       // Null values and streams are skipped
       if (file.isNull()) {
         return cb(null, file);
@@ -17,10 +24,64 @@ var tsPlugin = function (options) {
         return cb(new PluginError('gulp-ts', 'Streaming not supported'));
       }
 
-      var source = file.contents.toString('utf8');
-      
+      // Add file to the list of source files
+      files.push(file.path);
+    };
 
-    });
+    compileFiles = function () {
+      var compileCmd = files.join(' ');
+
+      console.log("Compiling " + compileCmd);
+
+      // Basic options
+      if (options.sourceMap) {
+        compileCmd += " --sourcemap";
+      }
+      if (options.declaration) {
+        compileCmd += " --declaration";
+      }
+      if (options.removeComments) {
+        compileCmd += "--removeComments";
+      }
+      if (options.noImplicitAny) {
+        compileCmd += "--noImplicitAny";
+      }
+      if (options.noResolve) {
+        compileCmd += "--noResolve";
+      }
+
+      // Module
+      cmd += ' --module ' + (options.module || 'amd').toLowerCase();
+
+      // Target ES3/ES5
+      cmd += ' --target ' + (options.target || 'ES3').toUpperCase();
+
+      // Output file
+      if (options.out) {
+        cmd += ' --out ' + options.out;
+      }
+
+      // Output directory
+      if (options.outDir) {
+        if (options.out) {
+          console.log("Warning: Don't use out with outDir!");
+        }
+        cmd += ' --outDir ' + options.outDir;
+      }
+
+      // Source root
+      if (options.sourceRoot) {
+        cmd += ' --sourceRoot' + options.sourceRoot;
+      }
+      
+      // Map root
+      if (options.mapRoot) {
+        cmd += ' --mapRoot' + options.mapRoot;
+      }
+    };
+
+
+    return through(bufferFiles, compileFiles)
 };
 
 
---
+++ test/tests.js
@@ -0,0 +1,13 @@
+/*jshint node:true */
+
+// Requires
+var gutil = require('gulp-util');
+var ts = require('../index');
+
+describe('gulp-ts', function () {
+  it('should compile one file', function () {
+
+  });
+});
+
+
